8d9f254d477d5a8168d748125a1e4cb979727232
supyuxiang <asd19780427@mail.dlut.edu.cn>
2026-08-03 23:45:11 +0800

    update

--- exp1_math/scripts/judge.sh
+++ exp1_math/scripts/judge.sh
@@ -1,11 +1,56 @@
 
 
+# CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
+#     --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
+#     --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_train_en_n8_t1.5_tokens1024.json \
+#     --save_path /root/autodl-tmp/exp1_math/judge/reward_math_train_en_n8_t1.5_tokens1024.pt
+
+# CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
+#     --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
+#     --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_test_en_n8_t1.5_tokens1024.json \
+#     --save_path /root/autodl-tmp/exp1_math/judge/reward_math_test_en_n8_t1.5_tokens1024.pt
+
+#####################    zh     #####################
 CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
     --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
-    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_train_en_n8_t1.5_tokens1024.json \
-    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_train_en_n8_t1.5_tokens1024.pt
+    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_train_zh_n8_t1.5_tokens1024.json \
+    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_train_zh_n8_t1.5_tokens1024.pt
 
 CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
     --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
-    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_test_en_n8_t1.5_tokens1024.json \
-    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_test_en_n8_t1.5_tokens1024.pt
+    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_test_zh_n8_t1.5_tokens1024.json \
+    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_test_zh_n8_t1.5_tokens1024.pt
+
+#####################    es     #####################
+CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
+    --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
+    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_train_es_n8_t1.5_tokens1024.json \
+    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_train_es_n8_t1.5_tokens1024.pt
+
+CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
+    --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
+    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_test_es_n8_t1.5_tokens1024.json \
+    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_test_es_n8_t1.5_tokens1024.pt
+
+#####################    vi     #####################
+CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
+    --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
+    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_train_vi_n8_t1.5_tokens1024.json \
+    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_train_vi_n8_t1.5_tokens1024.pt
+
+CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
+    --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
+    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_test_vi_n8_t1.5_tokens1024.json \
+    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_test_vi_n8_t1.5_tokens1024.pt
+
+#####################    tr     #####################
+CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
+    --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
+    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_train_tr_n8_t1.5_tokens1024.json \
+    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_train_tr_n8_t1.5_tokens1024.pt
+
+CUDA_VISIBLE_DEVICES=0 python /root/hidden_prob/exp1_math/judge.py \
+    --judge_model /root/autodl-tmp/models/Qwen2.5-14B-Instruct \
+    --data_path /root/hidden_prob/exp1_math/sampled/Qwen2.5-3B-Instruct/res_math_test_tr_n8_t1.5_tokens1024.json \
+    --save_path /root/autodl-tmp/exp1_math/judge/reward_math_test_tr_n8_t1.5_tokens1024.pt
+
